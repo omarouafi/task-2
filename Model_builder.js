327f4bb0928063955254c3225a389ef09b32fd3c
omarouafi <omarouafi@gmail.com>
2021-11-03 23:40:36 +0100

    task-2

--- Model_builder.js
+++ Model_builder.js
@@ -1,13 +1,38 @@
-let fs = require('fs');
+let fs = require("fs");
 
 function Model_builder() {
-  let config = fs.readFileSync('configuration.json');
+  let config = fs.readFileSync("configuration.json");
 
   this.build = function () {
     //generate files and put it into release folder
     //Copy initialize files into release folder
     //TODO
-  }
+    fs.mkdirSync("./release", {
+      recursive: true,
+    });
+
+    const models = JSON.parse(config).model;
+    models.map((model) => {
+      const fields = model.field;
+      const content = `<form method="POST" action="/api/${model.name}">
+      ${fields.map((field) => {
+        return `
+        <label>${field[2]}</label>
+        <input type="${field[1] === "integer" ? "number" : "text"}" name="${
+          field[0]
+        }" id="${field[0]}" value="" required=${field[3] === "required"}/>`;
+      })}
+      <input type="submit" name="submit" id="submit" value="Submit"/>
+      </form>;
+      `;
+      fs.writeFile(`./release/${model.name}.html`, content, (err) => {
+        if (err) console.log(err);
+      });
+    });
+  };
 
   return this;
 }
+
+const model = new Model_builder();
+model.build();
